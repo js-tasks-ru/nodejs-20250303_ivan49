21aa78208bc847162693c8de77a01b0f81a9c07d
ivan.auchynnikau <ivan.auchynnikau@itechart-group.com>
2025-03-17 15:49:57 +0300

    add solution for 02-nestjs-basics 01-tasks-controller

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
@@ -5,13 +5,47 @@ import { Task } from "./task.model";
 export class TasksService {
   private tasks: Task[] = [];
 
-  getAllTasks(): Task[] {}
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
 
-  getTaskById(id: string): Task {}
+  getTaskById(id: string): Task {
+    const index = this.tasks.findIndex((task) => task.id === id);
 
-  createTask(task: Task): Task {}
+    if (index < 0) throw new NotFoundException('Task not found!!!!');
 
-  updateTask(id: string, update: Task): Task {}
+    return this.tasks[index];
+  }
 
-  deleteTask(id: string): Task {}
+  createTask(task: Task): Task {
+    this.tasks.push({
+      id: this.tasks.length + 1 + '',
+      ...task
+    });
+
+    return this.tasks[this.tasks.length - 1];
+  }
+
+  updateTask(id: string, update: Task): Task {
+    const index = this.tasks.findIndex(task=> task.id === id);
+
+    if (index < 0) throw new NotFoundException('Task to update is not found!');
+
+    this.tasks[index] = update;
+    this.tasks[index].id = id;
+
+    return this.tasks[index];
+  }
+
+  deleteTask(id: string): Task {
+    const index = this.tasks.findIndex(task=> task.id === id);
+
+    if (index < 0) throw new NotFoundException('Task to delete is not found!');
+
+    const deletedTask = this.tasks[index];
+
+    this.tasks.splice(index, 1);
+
+    return deletedTask;
+  }
 }
