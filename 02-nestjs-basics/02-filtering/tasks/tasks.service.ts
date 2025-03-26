e80328fd8337ffec080c072029c0161de027d2a4
ivan.auchynnikau <ivan.auchynnikau@itechart-group.com>
2025-03-26 20:34:27 +0300

    add solution for 02-nestjs-basics/02-filtering

--- 02-nestjs-basics/02-filtering/tasks/tasks.service.ts
+++ 02-nestjs-basics/02-filtering/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { Task, TaskStatus } from "./task.model";
 
 @Injectable()
@@ -40,5 +40,42 @@ export class TasksService {
     status?: TaskStatus,
     page?: number,
     limit?: number,
-  ): Task[] {}
+  ): Task[] {
+      function splitArrayByLimit(arr: Task[], limit: number) {
+        const result = [];
+        for (let i = 0; i < arr.length; i += limit) {
+          result.push(arr.slice(i, i + limit));
+        }
+        return result;
+      }
+
+      let filteredByStatus = this.tasks;
+
+      if (status && Object.values(TaskStatus).indexOf(status) === -1) {
+        throw new BadRequestException('Use correct value for "status" !!!!');
+      }
+
+      if (status) filteredByStatus = this.tasks.filter(task => task.status === status);
+
+      if (page && isNaN(Number(page))) {
+        throw new BadRequestException('Use number for "page" !!!!');
+      }
+
+      if (limit && isNaN(Number(limit))) {
+        throw new BadRequestException('Use number for "limit" !!!!');
+      }
+
+      if (page && limit) {
+        const pageNum = Number(page);
+        const limitNum = Number(limit);
+
+        const splitedByPagesList = splitArrayByLimit(filteredByStatus, limitNum);
+
+        if (splitedByPagesList.length < pageNum) return [];
+
+        return splitedByPagesList[pageNum - 1];
+      }
+
+      return filteredByStatus;
+  }
 }
